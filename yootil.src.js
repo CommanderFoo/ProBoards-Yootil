46aaa82bac5628bc0ebb8af0ecb9bef5eece5de2
Chris Dessonville <cdville@mit.edu>
2012-11-06 17:32:38 -0500

    First run at locationcheck and useraction. v5 currently down, so some methods not yet implemented.

--- yootil.src.js
+++ yootil.src.js
@@ -678,3 +678,248 @@ yootil.user = (function(){
 	};
 	
 })();
+
+
+/**
+* Namespace: yootil.locationcheck
+*   Used to find out where we are on ProBoards
+*
+*   Later on we can expand this to have a URL generating section
+*/
+
+yootil.locationcheck = (function(){
+
+    return {
+
+        // Keep these sorted alphabetically. Just helps our sanity
+
+        /**
+        * Method: __is_page
+        *   INTERNAL METHOD.  Used to easily see if an id is the current page.
+        *
+        * Parameters:
+        *   id - *string* ID of the page to check route against
+        *
+        * Examples:
+        *   yootil.locationcheck.__is_page("board");
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        __is_page: function(id){
+            return proboards.data && proboards.data('route') && proboards.data('route').name == id;
+        },
+
+        /**
+        * Method: board
+        *   Are we currently viewing the main page of a board? (i.e. thread listing)
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        board: function(){
+            return this.__is_page('board');
+        },
+
+        /**
+        * Method: calendar
+        *   Are we currently viewing the main calendar page?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        calendar: function(){
+            return this.__is_page('calendar');
+        },
+
+        /**
+        * Method: forum
+        *   Are we currently viewing the main forum? (i.e. board listing)
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        forum: function(){
+            // CURRENT ISSUE: This is "forum" when custom page is homepage...
+            // See: http://support.proboards.com/index.cgi?board=openbetafeaturerequests&action=display&thread=429638&page=1
+            return this.__is_page('home');
+        },
+
+        /**
+        * Method: members
+        *   Are we currently viewing the members list?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        members: function(){
+
+        },
+
+        /**
+        * Method: message_list
+        *   Are we currently viewing the list of messages?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        message_list: function(){
+
+        },
+
+        /**
+        * Method: message_thread
+        *   Are we currently viewing a message?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        message_thread: function(){
+
+        },
+
+        /**
+        * Method: posting
+        *   Are we currently trying to post/create a thread/quote a post?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        posting: function(){
+            return this.posting_quote() || this.posting_reply() || this.posting_thread();
+        },
+
+        /**
+        * Method: posting_quote
+        *   Are we currently trying to reply with a quote?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        posting_quote: function(){
+            return this.__is_page('quotes_posts');
+        },
+
+        /**
+        * Method: posting_reply
+        *   Are we currently trying to post a reply?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        posting_reply: function(){
+            return this.__is_page('new_post');
+        },
+
+        /**
+        * Method: posting_thread
+        *   Are we currently trying to create a thread?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        posting_thread: function(){
+            return this.__is_page('new_thread');
+        },
+
+        /**
+        * Method: search
+        *   Are we currently trying to search?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        search: function(){
+            return this.__is_page('search');
+        },
+
+        /**
+        * Method: thread
+        *   Are we currently viewing a thread?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        thread: function(){
+            // View thread
+            return this.__is_page('thread');
+        }
+            
+    };
+    
+})();
+
+/**
+* Namespace: yootil.useraction
+*   Used to find out what actions a user has recently performed, in terms of the website usage.
+*/
+
+yootil.useraction = (function(){
+
+    return {
+
+        /**
+        * Method: created_thread
+        *   Did the user just create a new thread?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        created_thread: function(){
+
+        },
+
+        /**
+        * Method: posted
+        *   Did the user just post a new thread or reply?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        posted: function(){
+            // It is inclusive enough that they either created a thread or replied to one, to define posting
+            return this.created_thread() || this.replied();
+        },
+
+        /**
+        * Method: replied
+        *   Did the user just reply to a thread?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        replied: function(){
+
+        },
+
+        /**
+        * Method: sent_pm
+        *   Did the user just send a pm?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        sent_pm: function(){
+
+        }
+            
+    };
+    
+})();
