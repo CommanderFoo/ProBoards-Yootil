c515c614b19e0dae833da7c535122e4def387cfd
Chris Dessonville <cdville@mit.edu>
2012-11-07 11:51:44 -0500

    Bash compile script.  Does not execute min.

---
+++ bin/compile_src.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+# All inside the ./src dir
+# These are the file we want in yootil.src.js
+File[0]='yootil.js'
+File[1]='key.js'
+File[2]='create.js'
+File[3]='user.js'
+File[4]='ajax.js'
+File[5]='sound.js'
+File[6]='locationcheck.js'
+File[7]='useraction.js'
+
+# Clear source
+echo "" > yootil.src.js
+
+# Iterate over files and append
+for f in "${File[@]}"
+do
+cat "./src/$f" >> yootil.src.js
+echo "\n" >> yootil.src.js
+done
+
+echo "Done!"
