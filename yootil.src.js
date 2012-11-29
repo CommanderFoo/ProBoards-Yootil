d4157cb07b65d1358190dabc596bc9ac3b070471
Peter <github@pixeldepth.net>
2012-11-29 21:51:45 +0000

    Update min and src to latest versions

--- yootil.src.js
+++ yootil.src.js
@@ -1,5 +1,5 @@
 /**
-* Version: 0.7.2
+* Version: 0.7.3
 *
 * http://yootil.pixeldepth.net
 * http://pixeldepth.net
@@ -939,7 +939,7 @@ yootil.location.check = (function(){
 		},
 		
 		/**
-        * Method: convertation_new
+        * Method: conversation_new
         *   Are we currently creating a new conversation?
         *
         * Returns:
@@ -947,7 +947,7 @@ yootil.location.check = (function(){
         */
         
 		conversation_new: function(){
-			return this.__is_page("new_conversation");
+			return this.__is_page("new_conversation") || this.__is_page("create_conversation");
 		},
 
 		/**
@@ -1296,8 +1296,63 @@ yootil.location.check = (function(){
 */
 
 yootil.user.action = (function(){
+    var actionKey = "yootil.user.action.lastAction";
+    var current_action = "";
+
+    $(function(){
+        // Helper to shorten code
+        var markEvent = function(id){
+            return function(){
+                yootil.storage.set(actionKey, id);
+            };
+        };
+
+        // Pull our current action and then clear it
+        current_action = yootil.storage.get(actionKey);
+        yootil.storage.set(actionKey, "");
+
+        // There is only one case where a form submits without being validated client-side...
+        if(yootil.location.check.conversation_new() && current_action == "created_conversation"){
+            current_action = "";
+        }
+
+        // Onload, add our event handlers
+        $(".form_thread_new").submit(markEvent("created_thread"));
+        $(".form_post_new").submit(markEvent("replied")); // Gets quote also
+        $(".form_conversation_new").submit(markEvent("created_conversation"));
+        $(".form_message_new").submit(markEvent("sent_message"));
+
+        // So, the quick reply is a little janky.
+        if(yootil.location.check.message_thread()){
+            $(".form_post_quick_reply").submit(markEvent("sent_message"));
+        } else if(yootil.location.check.thread()){
+            $(".form_post_quick_reply").submit(markEvent("replied"));
+        }
+    });
+
+    /*
+        TODO:
+        - edited_post
+        - deleted_post/message
+        - added_poll
+        - added_event
+        - sent_message_quick_reply
+        - replied_quick_reply
+    */
 
     return {
+
+        /**
+        * Method: created_conversation
+        *   Did the user just create a new conversation?
+        *
+        * Returns:
+        *   *boolean*
+        */
+
+        created_conversation: function(){
+            return current_action == "created_conversation";
+        },
 
         /**
         * Method: created_thread
@@ -1308,7 +1363,19 @@ yootil.user.action = (function(){
         */
 
         created_thread: function(){
+            return current_action == "created_thread";
+        },
 
+        /**
+        * Method: pmed
+        *   Did the user just create a new conversation or send a message?
+        *
+        * Returns:
+        *   *boolean*
+        */
+        
+        pmed: function(){
+            return this.created_conversation() || this.sent_message();
         },
 
         /**
@@ -1320,7 +1387,6 @@ yootil.user.action = (function(){
         */
         
         posted: function(){
-            // It is inclusive enough that they either created a thread or replied to one, to define posting
             return this.created_thread() || this.replied();
         },
 
@@ -1333,19 +1399,19 @@ yootil.user.action = (function(){
         */
 
         replied: function(){
-
+            return current_action == "replied";
         },
 
         /**
-        * Method: sent_pm
-        *   Did the user just send a pm?
+        * Method: sent_message
+        *   Did the user just send a message/pm?
         *
         * Returns:
         *   *boolean*
         */
 
-        sent_pm: function(){
-
+        sent_message: function(){
+            return current_action == "sent_message";
         }
             
     };
