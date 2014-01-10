1d6e3d3b741610e924684ae8f7a4afaf3ccae399
Peter <github@pixeldepth.net>
2014-01-10 18:25:50 +0000

    Doc change

--- src/queue.js
+++ src/queue.js
@@ -1,13 +1,13 @@
 /**
 * Namespace: yootil.queue
-*	Wrapper around Queue object to handle queuing functions.
+*	Handle queuing functions.
 */
 
 yootil.queue = (function(){
 
 	/**
 	* Method: queue
-	*	Wrapper around Queue
+	*	Handle queuing functions.
 	*
 	* Parameters:
 	*	poll *integer* - How often should we poll the queue list (default is 100ms)?
@@ -16,15 +16,15 @@ yootil.queue = (function(){
 	*	*object* - Queue is returned
 	*
 	* Examples:
-	*	var q = new yootil.queue(); 
-	*	
+	*	var q = new yootil.queue();
+	*
 	*	q.add(function(){setTimeout(function(){console.log(1); q.next(); }, 1000)});
 	*
 	*	q.add(function(){setTimeout(function(){console.log(2); q.stop(); }, 1000)});
 	*
 	*	q.add(function(){setTimeout(function(){console.log(3); q.next(); }, 1000)}); // Won't run, as queue was stopped
 	*/
-		
+
 	function Queue(poll){
 		this.queue = [];
 		this.poll = poll || 100;
@@ -44,15 +44,15 @@ yootil.queue = (function(){
 		* Returns:
 		*	*object* - Queue is returned to allow chaining
 		*/
-		
+
 		add: function(func){
 			this.queue.push(func);
 			this.start();
-			
+
 			return this;
 		},
-		
-		start: function(){			
+
+		start: function(){
 			if(this.queue.length && !this.interval){
 				this.interval = setInterval($.proxy(function(){
 					if(!this.polling){
@@ -64,7 +64,7 @@ yootil.queue = (function(){
 				}, this), this.poll);
 			}
 		},
-		
+
 		/**
 		* Method: next
 		*	Move to the next item in the queue
@@ -72,20 +72,20 @@ yootil.queue = (function(){
 		* Returns:
 		*	*object* - Queue is returned to allow chaining
 		*/
-		
+
 		next: function(){
 			if(this.queue.length){
 				this.queue.shift();
 				this.polling = false;
 			}
-			
+
 			if(!this.queue.length){
 				clearInterval(this.interval);
 			}
-			
+
 			return this;
 		},
-		
+
 		/**
 		* Method: stop
 		*	Stop the queue
@@ -93,17 +93,17 @@ yootil.queue = (function(){
 		* Returns:
 		*	*object* - Queue is returned to allow chaining
 		*/
-		
+
 		stop: function(){
 			this.queue = [];
 			clearInterval(this.interval);
 			this.polling = this.interval = false;
-			
+
 			return this;
 		}
-		
+
 	};
-	
+
 	return Queue;
 
 })();
