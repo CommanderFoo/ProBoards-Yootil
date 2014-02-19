623aa3bf699811ee86d629f9b32c1998b6a38baa
Peter <github@pixeldepth.net>
2014-02-19 16:29:15 +0000

    Bug fix - user data null error

--- src/user.js
+++ src/user.js
@@ -11,9 +11,9 @@ yootil.user = (function(){
 		* Property: data
 		*	*object* Holds a reference to the ProBoards user object
 		*/
-		
+
 		data:  {},
-				
+
 		/**
 		* Method: has_data
 		*	This checks to see if the ProBoards data object exists and has a user object, we cache it as well.
@@ -21,26 +21,26 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		has_data: function(){
-			if(this.data && typeof this.data.id != "undefined"){
+			if(this.data && typeof this.data.id != "undefined"){console.log(1);
 				return true;
 			} else {
 				if(typeof proboards != "undefined"){
 					var data = proboards.data;
-					
+
 					if(typeof data != "undefined" && typeof data == "function"){
 						var user_data = proboards.data("user");
-						
+
 						if(typeof user_data != "undefined"){
-							this.data = user_data;
-							
+							this.data = user_data || {};
+
 							return true;
 						}
 					}
 				}
 			}
-			
+
 			return false;
 		},
 
@@ -51,17 +51,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		logged_in: function(){
 			if(this.has_data()){
 				if(typeof this.data.is_logged_in != "undefined" && this.data.is_logged_in){
 					return true;
 				}
 			}
-			
+
 			return false;
 		},
-		
+
 		/**
 		* Function: id
 		*	Gets the current users ID
@@ -69,17 +69,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		id: function(){
 			if(this.has_data()){
 				if(typeof this.data.id != "undefined"){
 					return this.data.id;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: is_staff
 		*	Checks to see if the current user is staff
@@ -87,17 +87,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		is_staff: function(){
 			if(this.has_data()){
 				if(typeof this.data.is_staff != "undefined" && this.data.is_staff){
 					return true;
 				}
 			}
-			
+
 			return false;
 		},
-		
+
 		/**
 		* Function: name
 		*	Gets the users name
@@ -105,17 +105,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		name: function(){
 			if(this.has_data()){
 				if(typeof this.data.name != "undefined"){
 					return this.data.name;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: theme
 		*	Gets the users theme ID
@@ -123,17 +123,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		theme: function(){
 			if(this.has_data()){
 				if(typeof this.data.theme_id != "undefined"){
 					return this.data.theme_id;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: url
 		*	Gets the users path URL to their profile
@@ -141,17 +141,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		url: function(){
 			if(this.has_data()){
 				if(typeof this.data.url != "undefined"){
 					return this.data.url;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: avatar
 		*	Gets the users avatar (HTML)
@@ -159,17 +159,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		avatar: function(){
 			if(this.has_data()){
 				if(typeof this.data.avatar != "undefined"){
 					return this.data.avatar;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: birthday
 		*	Gets the users birthday object
@@ -177,17 +177,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		birthday: function(){
 			if(this.has_data()){
 				if(typeof this.data.birthday != "undefined"){
 					return this.data.birthday;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: date_format
 		*	Gets the users date format (i.e d/m/y)
@@ -195,17 +195,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		date_format: function(){
 			if(this.has_data()){
 				if(typeof this.data.date_format != "undefined"){
 					return this.data.date_format;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: friends
 		*	Gets the users friends
@@ -213,17 +213,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		friends: function(){
 			if(this.has_data()){
 				if(typeof this.data.friends != "undefined"){
 					return this.data.friends;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: has_new_messages
 		*	Checks to see if user has new messages
@@ -231,17 +231,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		has_new_messages: function(){
 			if(this.has_data()){
 				if(typeof this.data.has_new_messages != "undefined"){
 					return this.data.has_new_messages;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: instant_messengers
 		*	Gets users instant messengers
@@ -249,17 +249,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		instant_messengers: function(){
 			if(this.has_data()){
 				if(typeof this.data.instant_messengers != "undefined"){
 					return this.data.instant_messengers;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: last_online
 		*	Gets users last online object
@@ -267,17 +267,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		last_online: function(){
 			if(this.has_data()){
 				if(typeof this.data.last_online != "undefined"){
 					return this.data.last_online;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: posts
 		*	Gets users post count
@@ -285,17 +285,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		posts: function(){
 			if(this.has_data()){
 				if(typeof this.data.posts != "undefined"){
 					return this.data.posts;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: rank
 		*	Gets users rank
@@ -303,17 +303,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		rank: function(){
 			if(this.has_data()){
 				if(typeof this.data.rank != "undefined"){
 					return this.data.rank;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: registered_on
 		*	Gets users registered on date
@@ -321,17 +321,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		registered_on: function(){
 			if(this.has_data()){
 				if(typeof this.data.registered_on != "undefined"){
 					return this.data.registered_on;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: status
 		*	Gets users status
@@ -339,17 +339,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		status: function(){
 			if(this.has_data()){
 				if(typeof this.data.status != "undefined"){
 					return this.data.status;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: time_format
 		*	Gets users time format
@@ -357,17 +357,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		time_format: function(){
 			if(this.has_data()){
 				if(typeof this.data.time_format != "undefined"){
 					return this.data.time_format;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: username
 		*	Gets users username
@@ -375,17 +375,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		username: function(){
 			if(this.has_data()){
 				if(typeof this.data.username != "undefined"){
 					return this.data.username;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: group_ids
 		*	Gets users group ids
@@ -393,17 +393,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*array*
 		*/
-		
+
 		group_ids: function(){
 			if(this.has_data()){
 				if(typeof this.data.group_ids != "undefined"){
 					return this.data.group_ids;
 				}
 			}
-			
+
 			return [];
 		},
-		
+
 		/**
 		* Function: groups
 		*	Gets users groups
@@ -411,17 +411,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		groups: function(){
 			if(this.has_data()){
 				if(typeof this.data.groups != "undefined"){
 					return this.data.groups;
 				}
 			}
-			
+
 			return {};
 		}
-		
+
 	};
-	
+
 })();
--- yootil.dev.js
+++ yootil.dev.js
@@ -1,5 +1,5 @@
 /**
-* Yootil Library: 0.9.5
+* Yootil Library: 0.9.6
 *
 * http://yootil.pixeldepth.net
 */
@@ -65,7 +65,7 @@ yootil = (function(){
 
 	return {
 
-		VERSION: "0.9.5",
+		VERSION: "0.9.6",
 
 		settings: {},
 
@@ -836,9 +836,9 @@ yootil.user = (function(){
 		* Property: data
 		*	*object* Holds a reference to the ProBoards user object
 		*/
-		
+
 		data:  {},
-				
+
 		/**
 		* Method: has_data
 		*	This checks to see if the ProBoards data object exists and has a user object, we cache it as well.
@@ -846,26 +846,26 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		has_data: function(){
-			if(this.data && typeof this.data.id != "undefined"){
+			if(this.data && typeof this.data.id != "undefined"){console.log(1);
 				return true;
 			} else {
 				if(typeof proboards != "undefined"){
 					var data = proboards.data;
-					
+
 					if(typeof data != "undefined" && typeof data == "function"){
 						var user_data = proboards.data("user");
-						
+
 						if(typeof user_data != "undefined"){
-							this.data = user_data;
-							
+							this.data = user_data || {};
+
 							return true;
 						}
 					}
 				}
 			}
-			
+
 			return false;
 		},
 
@@ -876,17 +876,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		logged_in: function(){
 			if(this.has_data()){
 				if(typeof this.data.is_logged_in != "undefined" && this.data.is_logged_in){
 					return true;
 				}
 			}
-			
+
 			return false;
 		},
-		
+
 		/**
 		* Function: id
 		*	Gets the current users ID
@@ -894,17 +894,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		id: function(){
 			if(this.has_data()){
 				if(typeof this.data.id != "undefined"){
 					return this.data.id;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: is_staff
 		*	Checks to see if the current user is staff
@@ -912,17 +912,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		is_staff: function(){
 			if(this.has_data()){
 				if(typeof this.data.is_staff != "undefined" && this.data.is_staff){
 					return true;
 				}
 			}
-			
+
 			return false;
 		},
-		
+
 		/**
 		* Function: name
 		*	Gets the users name
@@ -930,17 +930,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		name: function(){
 			if(this.has_data()){
 				if(typeof this.data.name != "undefined"){
 					return this.data.name;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: theme
 		*	Gets the users theme ID
@@ -948,17 +948,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		theme: function(){
 			if(this.has_data()){
 				if(typeof this.data.theme_id != "undefined"){
 					return this.data.theme_id;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: url
 		*	Gets the users path URL to their profile
@@ -966,17 +966,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		url: function(){
 			if(this.has_data()){
 				if(typeof this.data.url != "undefined"){
 					return this.data.url;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: avatar
 		*	Gets the users avatar (HTML)
@@ -984,17 +984,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		avatar: function(){
 			if(this.has_data()){
 				if(typeof this.data.avatar != "undefined"){
 					return this.data.avatar;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: birthday
 		*	Gets the users birthday object
@@ -1002,17 +1002,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		birthday: function(){
 			if(this.has_data()){
 				if(typeof this.data.birthday != "undefined"){
 					return this.data.birthday;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: date_format
 		*	Gets the users date format (i.e d/m/y)
@@ -1020,17 +1020,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		date_format: function(){
 			if(this.has_data()){
 				if(typeof this.data.date_format != "undefined"){
 					return this.data.date_format;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: friends
 		*	Gets the users friends
@@ -1038,17 +1038,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		friends: function(){
 			if(this.has_data()){
 				if(typeof this.data.friends != "undefined"){
 					return this.data.friends;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: has_new_messages
 		*	Checks to see if user has new messages
@@ -1056,17 +1056,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*boolean*
 		*/
-		
+
 		has_new_messages: function(){
 			if(this.has_data()){
 				if(typeof this.data.has_new_messages != "undefined"){
 					return this.data.has_new_messages;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: instant_messengers
 		*	Gets users instant messengers
@@ -1074,17 +1074,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		instant_messengers: function(){
 			if(this.has_data()){
 				if(typeof this.data.instant_messengers != "undefined"){
 					return this.data.instant_messengers;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: last_online
 		*	Gets users last online object
@@ -1092,17 +1092,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		last_online: function(){
 			if(this.has_data()){
 				if(typeof this.data.last_online != "undefined"){
 					return this.data.last_online;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: posts
 		*	Gets users post count
@@ -1110,17 +1110,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*integer*
 		*/
-		
+
 		posts: function(){
 			if(this.has_data()){
 				if(typeof this.data.posts != "undefined"){
 					return this.data.posts;
 				}
 			}
-			
+
 			return 0;
 		},
-		
+
 		/**
 		* Function: rank
 		*	Gets users rank
@@ -1128,17 +1128,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		rank: function(){
 			if(this.has_data()){
 				if(typeof this.data.rank != "undefined"){
 					return this.data.rank;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: registered_on
 		*	Gets users registered on date
@@ -1146,17 +1146,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		registered_on: function(){
 			if(this.has_data()){
 				if(typeof this.data.registered_on != "undefined"){
 					return this.data.registered_on;
 				}
 			}
-			
+
 			return {};
 		},
-		
+
 		/**
 		* Function: status
 		*	Gets users status
@@ -1164,17 +1164,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		status: function(){
 			if(this.has_data()){
 				if(typeof this.data.status != "undefined"){
 					return this.data.status;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: time_format
 		*	Gets users time format
@@ -1182,17 +1182,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		time_format: function(){
 			if(this.has_data()){
 				if(typeof this.data.time_format != "undefined"){
 					return this.data.time_format;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: username
 		*	Gets users username
@@ -1200,17 +1200,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*string*
 		*/
-		
+
 		username: function(){
 			if(this.has_data()){
 				if(typeof this.data.username != "undefined"){
 					return this.data.username;
 				}
 			}
-			
+
 			return "";
 		},
-		
+
 		/**
 		* Function: group_ids
 		*	Gets users group ids
@@ -1218,17 +1218,17 @@ yootil.user = (function(){
 		* Returns:
 		*	*array*
 		*/
-		
+
 		group_ids: function(){
 			if(this.has_data()){
 				if(typeof this.data.group_ids != "undefined"){
 					return this.data.group_ids;
 				}
 			}
-			
+
 			return [];
 		},
-		
+
 		/**
 		* Function: groups
 		*	Gets users groups
@@ -1236,19 +1236,19 @@ yootil.user = (function(){
 		* Returns:
 		*	*object*
 		*/
-		
+
 		groups: function(){
 			if(this.has_data()){
 				if(typeof this.data.groups != "undefined"){
 					return this.data.groups;
 				}
 			}
-			
+
 			return {};
 		}
-		
+
 	};
-	
+
 })();
 
 /**
