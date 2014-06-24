b110a49282e56b5273c4279a88292f41331c576f
ProTex <petermaggio@sbcglobal.net>
2014-06-24 17:33:53 -0500

    Removed __remove and removed/simplified
    Removed function "__remove" and object "removed". Fixed some syntax
    problems and switched to hiding the items instead of removing them. Also
    removed all error throws.

--- src/remove.js
+++ src/remove.js
@@ -6,58 +6,7 @@
 
 yootil.remove = (function(){
 
-	return {
-
-		/**
-		* Object: removed
-		*   Containes all removed objects, eventually will support easy insert back into DOM
-		*/
-
-		removed: {},
-
-		/**
-		* Function: __remove
-		*	This is an interal method
-		*
-		* Parameters:
-		*	Obj - *object* The object to be removed		
-		*   where - *string* Name of function used to remove the object (Example: 'nav_tree')
-		*   selector - *string* Selector used in the "where" function (Example: '[href="/members"]')
-		*
-		* Example:
-		*   yootil.remove.__remove($(''[href="/members"]'), 'nav_branch', '[href="/members"]'))
-		*
-		* Returns:
-		*	*object* yootil.remove
-		*/
-
-		__remove: function (obj, where, selector) {
-
-			var	id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1),
-				clone = obj.clone(true, true);
-
-			if ( obj.length ) {			
-
-				obj.replaceWith('<div id="' + id + '" style="display: none"/>');
-
-				if(this.removed[where]) {
-					this.removed[where][selector] = { "obj": clone, "id": id };
-				}
-				else{
-					this.removed[where] = {};
-					this.removed[where][selector] = { "obj": clone, "id": id };
-				}
-
-			} else {
-
-				var err = new Error();
-
-				throw err;
-
-			}
-
-
-		},			
+	return {			
 
 		/**
 		* Function: nav_item
@@ -67,7 +16,7 @@ yootil.remove = (function(){
 		*	Args - *string* / *array* The exact url of the Nav Item to be removed
 		*		
 		* Returns:
-		*	*object* yootil.remove
+		*	*array* Contains all removed objects for easy return
 		*
 		* Examples:
 		*	yootil.remove.nav_item('Forum');
@@ -77,34 +26,24 @@ yootil.remove = (function(){
 
         nav_branch: function (args) {
 
-            var array
-                _nav = $('#nav-tree');
+            var array,
+                nav = $('#nav-tree'),
+                removed = [];
 
             if ( Object.prototype.toString.call(args) !== "[object Array]" )
                 array = new Array(args);
             else
                 array = args;
 
-            for ( i in array ) { 
+            for ( var i in array ) { 
 
-            	var obj = _nav.find('[href="' + array[i].toString() + '"]').parentsUntil('#nav-tree');
+            	var obj = nav.find('[href="' + array[i].toString() + '"]').parentsUntil('#nav-tree').hide();
 
-            	try{
-
-		            this.__remove(obj, 'nav_item', '[href="' + array[i].toString() + '"]');
-
-		        } catch (e) {
-
-		        	var err = new Error('Tried to remove a nav branch that did not exist.');
-		        	err.name = "Yootil Error";
-
-		        	throw err;
-
-		        }
+         		removed.push(obj);
 
             }       
 
-            return yootil.remove;
+            return removed;
         },	
 
 	}
