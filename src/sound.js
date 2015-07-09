626ec8aafbda5fa3eab1f8646758c3dffc8540d7
Peter <github@pixeldepth.net>
2015-07-09 12:19:21 +0100

    sound module ignored from docs and no longer in the build for 1.0.0

--- src/sound.js
+++ src/sound.js
@@ -1,6 +1,7 @@
 /**
  * @class yootil.sound
  * @static
+ * @ignore
  * Allows us to play a sound (uses HTML 5 Audio).
  *
  * Set access on the audio files, specifically Access-Control-Allow-Origin.
